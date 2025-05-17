ac1f28f624cf0f7a6f91e620047ace12187e170f
Morsi Youcef <141324213+MorsiYoucef@users.noreply.github.com>
2025-05-17 15:22:09 +0100

    chore: some changes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import config from './config/config';
 
+const port = config.port || 3000;
+
 app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
+  console.log(`Server running on port ${port}`);
 });
