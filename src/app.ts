4f6dd58dfa163daad41518fae600e478a7a5fba9
Morsi Youcef <141324213+MorsiYoucef@users.noreply.github.com>
2025-06-27 19:22:46 +0100

    adding rate limiter and redis config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express, { NextFunction, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import helmet from 'helmet';
+import Redis from 'ioredis';
+import { rateLimit } from 'express-rate-limit';
+import { RedisStore } from 'rate-limit-redis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
 import cookieParser from 'cookie-parser';
-import AppRoutes from './routes/router'
+import AppRoutes from './routes/router';
 import passport from 'passport';
 import logger from './utils/logger';
 import { errorHandler } from './middlewares/errorHandler';
@@ -11,16 +15,18 @@ import { applyGraphQL } from './graphql';
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true
-}));
-// app.use(passport.initialize());
-// app.use(passport.session());
+const redisClient = new Redis(process.env.REDIS_URL as string);
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(helmet())
+app.use(helmet());
 
 app.use((req, res, next) => {
   logger.info(`Received ${req.method} request to ${req.url}`);
@@ -28,13 +34,39 @@ app.use((req, res, next) => {
   next();
 });
 
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'middleware',
+  points: 100, // 10 requests per minute
+  duration: 60, // per minute
+});
+
+app.use((req, res, next) => {
+  rateLimiter
+    .consume(req.ip as string)
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      logger.warn(`Rate limit exceeded for IP: ${req.ip}`);
+      res.status(429).send('Too Many Requests');
+    });
+});
+
 // Apply GraphQL endpoint
 applyGraphQL(app);
 
 // Routes
-app.use('/api', AppRoutes);
+app.use(
+  '/api',
+  (req: Request, res: Response, next: NextFunction) => {
+    req.RedisClient = redisClient;
+    next();
+  },
+  AppRoutes,
+);
 
-app.use(errorHandler);  
+app.use(errorHandler);
 // Global error handler (should be after routes)
 // app.use(errorHandler);
 
